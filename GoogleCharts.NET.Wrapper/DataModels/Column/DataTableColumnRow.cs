2dde30d24ecfed406628d566b967442049f08279
Anton Ruvinsky <anton.ruvinsky@t-a9.com>
2022-03-30 11:17:39 +0300

    Breaking change: All charts components now called as GoogleChart. (DataTables Type will change type of chart)

---
+++ GoogleCharts.NET.Wrapper/DataModels/Column/DataTableColumnRow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCharts.NET.Wrapper.DataModels.Column
+{
+    public class DataTableColumnRow
+    {
+    }
+}
